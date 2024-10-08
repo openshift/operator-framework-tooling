ed109abd8d96c4826cfaf60e8c1f645b3cb47af4
Mikalai Radchuk <mradchuk@redhat.com>
2024-10-08 12:00:42 +0200

    Remove most of the assumptions from examples
    Makes a bit easier to run the script because you no longer
    need to manually run `make` before running the script
    and you don't have to checkout tooling repo
    into a specific path.
    
    Signed-off-by: Mikalai Radchuk <mradchuk@redhat.com>

--- examples/downstream-v0.sh
+++ examples/downstream-v0.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
-#
-# This script will run a local OLMv0 downstream operation, to allow someone to
-# debug a failure.
-#
-# Assumptions:
-# * The tooling directory is located in ${HOME}/git/openshift/operator-framework-tooling
-# * The v0 executable has been built in there via `make`
-#
-# Call with your github user name, if it's not the same as your current user name.
-#
+set -euo pipefail
 
-GITHUB_USER=${1:-${USER}}
+usage() {
+    echo "This script will run a local OLMv0 downstream operation, to allow someone to"
+    echo "debug a failure."
+    echo ""
+    echo "Usage: $0 [-u github-username] [-a \"-extra-sync args\"] [-d /path/to/the/sync/dir]"
+    echo ""
+    echo "Options:"
+    echo " -u GitHub username. Default: current system user."
+    echo " -a Additional arguments to the syncer."
+    echo " -d Path to the sync dir. Default: current working directory."
+    echo ""
+}
+
+GITHUB_USER=$USER
+SYNC_ARGS=""
+SYNC_DIR=$PWD
+
+# Get the options
+while getopts "hu:a:d:" option; do
+   case $option in
+      u)
+        GITHUB_USER=$OPTARG;;
+      a)
+        SYNC_ARGS=$OPTARG;;
+      d)
+        SYNC_DIR=$OPTARG;;
+      h)
+        usage
+        exit 1;;
+     \?)
+        usage
+        exit 1;;
+   esac
+done
+
+# Get an absolute path of the root of the tooling repo
+# based on the path of this script.
+TOOLS_REPO_DIR=$(dirname "$( cd $(dirname $0) ; pwd)")
 
 # Cleanup from last time
 reset-repo () {
@@ -26,6 +54,7 @@ reset-repo () {
 }
 
 setup-repo() {
+    pushd $SYNC_DIR
     if [ -d $1 ]; then
         echo "Resetting $1"
         (cd $1 && reset-repo)
@@ -34,14 +63,19 @@ setup-repo() {
         git clone git@github.com:openshift/$1.git
         git -C $1 remote add ${GITHUB_USER} git@github.com:${GITHUB_USER}/$1.git
     fi
+    popd
 }
 
 setup-repo operator-framework-olm
 
+make -C $TOOLS_REPO_DIR
+
 set -x
 
-cd operator-framework-olm
-${HOME}/git/openshift/operator-framework-tooling/v0 \
+pushd $SYNC_DIR/operator-framework-olm
+${TOOLS_REPO_DIR}/v0 \
        --mode=synchronize \
        --delay-manifest-generation \
-       --log-level=debug
+       --log-level=debug \
+       ${SYNC_ARGS}
+popd
